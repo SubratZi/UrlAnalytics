36c8781da815cb29e247189eed4f6c946e6d53d9
subratzi <167219761+SubratZi@users.noreply.github.com>
2026-07-09 08:01:13 +0545

    chores: update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     allow_origins=["http://localhost:5173",
                    "http://urlanalytics.vercel.app"],
     allow_credentials = True,
+    allow_orgins=["*"]
     allow_methods=["*"],
     allow_headers=["*"],
 )
