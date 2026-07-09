212c3833cad58580ac88fa1824633575ed61ab46
subratzi <167219761+SubratZi@users.noreply.github.com>
2026-07-09 07:51:52 +0545

    chore: missing parameters added

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 10
+ACCESS_TOKEN_EXPIRE_MINUTES = 60*24*7
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated = "auto")
 
@@ -29,7 +29,7 @@ def create_access_token(data:dict) -> str:
 
 def decode_token(token: str) -> Optional[str]:
     try:
-        payload = jwt.decode(token, SECRET_KEY)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload.get('sub')
     except JWTError:
         return None
